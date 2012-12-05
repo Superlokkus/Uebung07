f9df898456560b6a1a0b382a5518538983f696db
Markus Klemm <markus@markusklemm.net>
2012-12-05 14:34:52 +0100

    Initial

---
+++ U07.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# encoding=utf-8
+
+import numpy as np
+
+"""Übung 07 Algebra mit numpy"""
+#Markus Klemm WS12/13 Phy-BA
+
+#Gegeben:
+(x1,y1) = (-1.5,10.975)
+(x2,y2) = (-0.4,13.008)
+(x3,y3) = (1.9,0.843)
+###
+
+
